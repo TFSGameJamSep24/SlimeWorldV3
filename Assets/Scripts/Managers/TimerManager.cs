a5f8e952241ce66ee9bb0b9f480928445f40e518
jowrites <jodie.keene@torontofilmschool.ca>
2024-10-02 11:34:08 -0400

    timer and slimes pop

--- Assets/Scripts/Managers/TimerManager.cs
+++ Assets/Scripts/Managers/TimerManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TimerManager : MonoBehaviour
 {
+
+    public delegate void Timer(float currentTime);
+    public event Timer OnTimerUpdate;
+
+
     public static TimerManager Instance { get; private set; }
 
     public float levelTime = 300f; // 5 mins
@@ -59,6 +64,7 @@ public class TimerManager : MonoBehaviour
             GameManager.Instance.EndGame();
         }
 
+        OnTimerUpdate?.Invoke(currentTime);
     }
 
     public void ResetTimer()
--- Assets/Scripts/Slime/SlimeMovement.cs
+++ Assets/Scripts/Slime/SlimeMovement.cs
@@ -9,24 +9,29 @@ public class SlimeMovement : MonoBehaviour
 
     [Header("Slime Movement Properties")]
     [SerializeField] private float speed = 2f;
-    [SerializeField] private float squishAmount = 0.5f;
+    //[SerializeField] private float squishAmount = 0.5f;
     [SerializeField] private float squishSpeed = 0.5f;
     [SerializeField] private float rotationSpeed = 2f;
+    [SerializeField] private float maxVelocity = 10f;
 
     private Vector3 originalScale;
     private Rigidbody rb;
+    private Animator anim;
     private Coroutine wobbleCoroutine;
     private Vector3 randomDirection;
+
 
     private void Start()
     {
         originalScale = transform.localScale;
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
         StartCoroutine(ChangeDirectionRoutine());
     }
 
     private void Update()
     {
+        
         //Calculate movement direction relative to gravity
         Vector3 gravityDirection = (GravitySource.instance.transform.position - transform.position).normalized;
         Vector3 right = Vector3.Cross(gravityDirection, transform.forward).normalized;
@@ -41,7 +46,7 @@ public class SlimeMovement : MonoBehaviour
             Quaternion targetRotation = Quaternion.LookRotation(movement, gravityDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
-            if (wobbleCoroutine == null)
+            /*if (wobbleCoroutine == null)
             {
                 wobbleCoroutine = StartCoroutine(WobbleEffect());
             }
@@ -52,10 +57,21 @@ public class SlimeMovement : MonoBehaviour
             {
                 StopCoroutine(wobbleCoroutine);
                 wobbleCoroutine = null;
-            }
+            }*/
 
             transform.localScale = Vector3.Lerp(transform.localScale, originalScale, Time.deltaTime * squishSpeed);
         }
+
+        if (rb.velocity.magnitude > maxVelocity)
+        {
+            anim.SetTrigger("Pop");
+        }
+    }
+
+    public void PopSlime()
+    {
+        gameObject.SetActive(false);
+
     }
 
 
@@ -68,16 +84,16 @@ public class SlimeMovement : MonoBehaviour
         }
     }
 
-    private IEnumerator WobbleEffect()
-    {
-        while (true)
-        {
-            float squishFactor = 1 - Mathf.Sin(Time.time * squishSpeed) * squishAmount;
-            Vector3 newScale = new Vector3(originalScale.x * squishFactor, originalScale.y, originalScale.z * squishFactor);
-            transform.localScale = Vector3.Lerp(transform.localScale, newScale, Time.deltaTime * squishSpeed); 
+    /* private IEnumerator WobbleEffect()
+     {
+         while (true)
+         {
+             float squishFactor = 1 - Mathf.Sin(Time.time * squishSpeed) * squishAmount;
+             Vector3 newScale = new Vector3(originalScale.x * squishFactor, originalScale.y, originalScale.z * squishFactor);
+             transform.localScale = Vector3.Lerp(transform.localScale, newScale, Time.deltaTime * squishSpeed); 
 
-            yield return null;
-        }
-    }
+             yield return null;
+         }
+     }*/
 
 }
